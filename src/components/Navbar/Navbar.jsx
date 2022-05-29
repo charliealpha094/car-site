a5c6feb661c4865745eca307521791b48287b1ca
charliealpha094 <carlosamaral94@gmail.com>
2022-05-29 21:18:39 +0100

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./NavbarStyle.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <h3>4x4 sales</h3>
+        </div>
+
+        <ul className=""></ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
