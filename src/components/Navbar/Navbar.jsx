827788d7fe8e9b2872fbbb70d87b63a5fd10bb80
charliealpha094 <carlosamaral94@gmail.com>
2022-05-29 23:43:10 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,26 @@ const Navbar = () => {
           <h3>4x4 sales</h3>
         </div>
 
-        <ul className=""></ul>
+        <ul className="nav-menu">
+          <li>
+            <a href="/">4Runner</a>
+          </li>
+          <li>
+            <a href="/">G</a>
+          </li>
+          <li>
+            <a href="/">Land Cruiser</a>
+          </li>
+          <li>
+            <a href="/">Pajero</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <li>
+            <a href="/">Accessories</a>
+          </li>
+        </ul>
       </div>
     </div>
   );
